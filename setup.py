7132ebe2e58202295c318e72eccbc6ed175d3724
Khai Do <3697686+zaro0508@users.noreply.github.com>
2021-04-14 20:38:48 -0700

    fix the build (#7)
    * bump to new version
    * replace docker image to latest sceptre-circleci image
    * remove sonar
    * use pre-commit
    * setup multiple circleci workflows

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "1.0.0"
+__version__ = "1.0.3"
 
 # More information on setting values:
 # https://github.com/Sceptre/project/wiki/sceptre-file-resolver
