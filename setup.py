f7f8252b35d11e89f5bdfd5795ca6c9afe3e4287
Khai Do <zaro0508@gmail.com>
2021-07-05 08:54:58 -0700

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 # More information on setting values:
 # https://github.com/Sceptre/project/wiki/sceptre-file-resolver
